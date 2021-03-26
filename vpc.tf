e8ed271aff65a17a5077cf01187599401472c32d
codepipe <47555726+codepipe@users.noreply.github.com>
2021-03-26 16:59:03 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA5KOY4NEOEZ4VCKGU"
+  secret_key = "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"
+}
+
+resource "aws_vpc" "demo" {
+  cidr_block       = "66.66.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+resource "aws_subnet" "demo1"
+vpc_id = ${"aws_vpc.demo.id}
+cidr_block = "66.66.1.0./28"
+  
+  tags = {
+   Name = "subnet1"
+   }
+}
+resource "aws_subnet" "demo1"
+vpc_id = ${"aws_vpc.demo.id}
+cidr_block = "66.66.1.16./28"
+  
+  tags = {
+   Name = "subnet2"
+   }
+}
