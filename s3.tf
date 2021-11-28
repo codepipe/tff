d80ce134cc87041532f491c2a460e38c7578cae5
codepipe <47555726+codepipe@users.noreply.github.com>
2021-11-28 14:37:43 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "bucklebuck" {
   bucket = "my-tf-massiveprince-bucket"
   acl    = "private"
 
   tags = {
-    Name        = "ravi-daya-santhosh-deepak1231"
+    Name        = "rav1i-day1a-santhos1h-deepak1231"
     Environment = "Dev"
   }
 }
